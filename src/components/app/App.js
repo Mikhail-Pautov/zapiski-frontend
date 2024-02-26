970f66d7ac7e270332331e2e68f179a8fff8ed18
Mikhail <89242326+Mikhail-Pautov@users.noreply.github.com>
2024-02-26 17:26:04 +0300

    Update App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -32,7 +32,7 @@ const App = () => {
                 {/* <Route path="*" element={<Login/>} />    */}
              </Routes> 
             
-          {/*   <CreateNote/> */}
+             <CreateNote/> 
         </main>
     )
 }
